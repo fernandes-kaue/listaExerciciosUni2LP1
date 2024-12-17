5fe7711ac0ef53bd49bc182f2d84bf3b94a07e50
Kauê Fernandes <90159661+fernandes-kaue@users.noreply.github.com>
2024-12-17 16:14:47 -0300

    correção de tamanho de array para evitar nullPointerException

--- src/Loja.java
+++ src/Loja.java
@@ -1,22 +1,19 @@
 public class Loja {
     public static void main(String[] args) {
-        Produto[] produtos = new Produto[15];
+        Produto[] produtos = new Produto[9];
 
-        produtos[8] = new Livro("A Casa de Folhas", 333, "666", "Mark Z. Danielewski");
+        produtos[8] = new Livro("A Casa de Folhas", 333, "656", "Mark Z. Danielewski");
         produtos[7] = new Livro("A nausea", 222, "3210", "Sartre");
         produtos[6] = new Livro("A Casa de Folhas", 333, "666", "Mark Z. Danielewski");
 
-        produtos[5] = new CD("The Marshall Mather LP", 99, "1999", 15);
+        produtos[5] = new CD("The Marshall Mather LP", 99, "1989", 15);
         produtos[4] = new CD("Music To Be Murdered By", 100, "2022", 24);
         produtos[3] = new CD("The Marshall Mather LP", 99, "1999", 15);
 
-        produtos[2] = new DVD("O Rei Leao", 33, "2003", 2.5);
+        produtos[2] = new DVD("O Rei Leao", 33, "2004", 2.5);
         produtos[1] = new DVD("Interestelar", 123, "2014", 3.3);
-        produtos[0] = new DVD("O Rei Leao", 33, "2003", 2.5);
+        produtos[0] = new DVD("O Rei Leao", 33, "2013", 2.5);
 
-        for (int i = 9; i < produtos.length; i++) {
-            produtos[i] = new Produto("nulo", 0, "-1");
-        }
 
         Produto produtoBuscado = new Livro("A Casa de Folhas", 333, "666", "Mark Z. Danielewski");
         Produto produtoBuscado1 = new Livro("A Casa de Folhas", 333, "766", "Mark Z. Danielewski");
